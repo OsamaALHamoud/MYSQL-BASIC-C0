bb81c3123563aba0e326b0b15cbd4a8820569aeb
OsamaALhamoud <osama.alhamoud@student.rocva.nl>
2021-03-16 13:40:35 +0100

    voldaan

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,15 @@
 -- Opdracht 1 - voorbeeld
-SELECT * FROM jaar2016
+SELECT `country` FROM `jaar2015` WHERE `rank`=7
 -- Opdracht 2 
-
+SELECT `country`,`rank` FROM `jaar2015` WHERE rank=7
 -- Opdracht 3
-
+SELECT country, score FROM `jaar2015` WHERE score = 7
 -- Opdracht 4
 
 -- Opdracht 5
+SELECT country,region FROM `jaar2016` WHERE region = "North America"
 
 -- Opdracht 6
-
+SELECT `country` FROM `jaar2016` WHERE `rank`=25
 -- Opdracht 7
+SELECT `country` FROM `jaar2016` WHERE score >7087
