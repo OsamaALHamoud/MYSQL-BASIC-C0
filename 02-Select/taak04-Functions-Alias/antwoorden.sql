ccc725be4a085b8a5cba0e4688943ec643970e6b
OsamaALhamoud <osama.alhamoud@student.rocva.nl>
2021-03-30 12:58:08 +0100

    take 4 voldaan

--- 02-Select/taak04-Functions-Alias/antwoorden.sql
+++ 02-Select/taak04-Functions-Alias/antwoorden.sql
@@ -1,19 +1,30 @@
 -- Opdracht 1 
+SELECT name, club, MAX(wage) AS avg_wage FROM players WHERE club = "FC Utrecht"
 
 -- Opdracht 2 
+SELECT ROUND(wage) FROM players WHERE 1
 
 -- Opdracht 3
+SELECT SUM(wage) FROM players WHERE club="FC Groningen"
 
 -- Opdracht 4
+SELECT name, nationality, Round(wage) AS Roun_wage FROM players WHERE `nationality`="Netherlands"
 
 -- Opdracht 5
+SELECT name, nationality, Round(wage) AS Roun_wage FROM players WHERE `nationality`="Netherlands"
 
 -- Opdracht 6 
+SELECT name , club, age ,Round(wage) AS Roun_wage FROM players WHERE age <= 20
 
 -- Opdracht 7 
+SELECT name , club, age ,Round(wage) AS Roun_wage FROM players WHERE age >= 20
 
 -- Opdracht 8
+SELECT club , COUNT(club) FROM players WHERE club="Chelsea"
 
 -- Opdracht 9
+SELECT name, club, Round(age) FROM players WHERE 1
 
 -- Opdracht 10
+
+SELECT SUM(wage) FROM players WHERE club="Luverpool"
